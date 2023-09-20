c43f15fd9c968576b801e2abf38fdfcb424680ad
Sudhanshu00052 <132894325+Sudhanshu00052@users.noreply.github.com>
2023-09-20 16:40:37 +0000

    test1.js

--- 01-basic/test1.js
+++ 01-basic/test1.js
@@ -1,1 +1,7 @@
-
+const accountId=1234;
+let accountEmail='skshgafcfgv@gmail.com';
+accountPassword='234232323'
+let accountCity='bulandshahr'
+let accountState;
+ console.table([accountPassword, accountEmail,accountCity,accountId,accountState]);
+console.log(accountPassword);
