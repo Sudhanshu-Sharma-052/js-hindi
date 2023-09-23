3f5918bd2f5ce12dc2d443f254ec2d9eda6f9f2c
Sudhanshu00052 <132894325+Sudhanshu00052@users.noreply.github.com>
2023-09-23 18:08:33 +0000

    t2.js

---
+++ 01-basic/t2.js
@@ -0,0 +1,12 @@
+//"use strict";
+//alert(3+3);
+//console.log('ram'); 
+//console.log(3+3);
+firstName="ram";
+lastName="sharma";
+let name="nikki";;
+let age=18;
+let isLogedIn=true;
+let state=null;
+console.log(typeof"sharma")
+console.table([firstName,lastName]);
