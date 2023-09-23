ea47afdf1d1c95cf80b81d04d9337530f35ff829
Sudhanshu00052 <132894325+Sudhanshu00052@users.noreply.github.com>
2023-09-23 18:09:00 +0000

    t3.js

---
+++ 01-basic/t3.js
@@ -0,0 +1,15 @@
+//let score="33abc";
+//let score=null
+//let score = undefined
+let score=true
+console.log(typeof (score));
+//console.log(typeof score);
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
