2307c6b8581265f2db7da599a006807f1477de36
Sudhanshu00052 <132894325+Sudhanshu00052@users.noreply.github.com>
2023-09-30 15:45:57 +0000

    container config done

---
+++ 01-basic/ar77.js
@@ -0,0 +1,35 @@
+//Array..........
+
+const myArr = [0,1,2,3,4,5];
+//console.log(myArr);
+//const myHeros = ["shakti","spider","krish"];
+
+//const myArr2 = new Array(1,2,3,4);
+//console.log(myArr[1]);
+
+//Methods.....
+//myArr.push(6,7);
+//console.log(myArr);
+//myArr.unshift(9);
+//myArr.shift(9);
+//console.log(myArr);
+//console.log(myArr.includes(9));
+//console.log(myArr);
+//console.log(myArr.includes(2));
+//console.log(myArr);
+//console.log(myArr.indexOf(2));
+//console.log(myArr);
+
+//const newArr = myArr.join();
+//console.log(myArr);
+//console.log(newArr);
+//console.log(typeof newArr);
+
+//SLICE , SPLICE....
+console.log("A", myArr);
+const myna1 = myArr.slice(1,3);
+console.log(myna1);
+console.log("B", myArr);
+
+const myna2 = myArr.splice(1,3);
+console.log(myna2);
